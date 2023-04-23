b0ed3fb8128b6aa70bd54e7a4f6d078620db0293
campfire5 <barbaqadzedato3@gmail.com>
2023-04-23 19:59:03 +0400

    feat: added contacts provider for cleaner interface

--- lib/src/widgets/contacts_provider.dart
+++ lib/src/widgets/contacts_provider.dart
@@ -1,1 +1,22 @@
+import 'package:contacts_provider/contacts_provider.dart';
+import 'package:contacts_provider/src/widgets/inherited_contacts.dart';
+import 'package:flutter/material.dart';
 
+class ContactsProvider extends StatelessWidget {
+  final Contacts Function(BuildContext) create;
+  final Widget child;
+  const ContactsProvider({
+    super.key,
+    required this.create,
+    required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final Contacts contacts = create(context);
+    return InheritedContacts(
+      contacts: contacts,
+      child: child,
+    );
+  }
+}
